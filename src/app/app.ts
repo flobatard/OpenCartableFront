87ace93ddf8249188a6bdde07ff03e2f0a616a75
flobatard <fb.batard@gmail.com>
2026-07-02 17:04:41 +0200

    Renaming project to OpenCartable

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('CartableFront');
+  protected readonly title = signal('OpenCartableFront');
 }
